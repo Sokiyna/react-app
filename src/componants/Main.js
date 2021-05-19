a074e6af777e304ba8a006874c00b4438e1a70ed
Sokiyna <sokiynanaser@yahoo.com>
2021-05-19 22:05:42 +0300

    upload the lab4

--- src/componants/Main.js
+++ src/componants/Main.js
@@ -2,13 +2,15 @@ import React from 'react';
 import HornedBeasts from './HornedBeasts';
 import dataArr from "./data.json";
 import CardColumns from 'react-bootstrap/CardColumns';
+import Form from 'react-bootstrap/Form';
+
 
 
 
 
 class Main extends React.Component {
 
-
+    
     // constructor(props) {
     //     super(props);
     //     this.state = {
@@ -22,35 +24,65 @@ class Main extends React.Component {
 
     // showImg
 
+    hornsFilter = (event) => {
 
+        let optionNum = event.target.value;
+        let arrCopy = dataArr;
+        let arr = [];
+        if (optionNum) {
+            arr = arrCopy.filter(item => {
 
-    render() {
+                if (item.horns == optionNum) {
+                    return item;
+                } else{return''}
 
-        return (
-            <CardColumns>
+                
+                
+            })
 
-<>
-                {dataArr.map((items, index) => {
-                    return (
-                        <HornedBeasts
-                            key={index}
-                            title={items.title}
-                            imageUrl={items.image_url}
-                            description={items.description}
-                            showHandle={this.props.showHandle} />
-
-                    )
-                }
-
-                )}
-
-
-
-            </>
-            </CardColumns>
-           
-        )
+        } else{
+            arr=arrCopy;
+        }
+        this.props.filterAnimals(arr);
     }
-}
 
-export default Main;
+
+
+        render(){
+
+            return (
+                
+                     <div>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Horns Number</Form.Label>
+                        <Form.Control as="select" custom onChange={this.hornsFilter} >
+                            <option value="">All</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                            <option value="100">WooW</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <CardColumns>
+                    <>
+                        {this.props.dataArr.map((items, index) => {
+                            return (
+                                <HornedBeasts
+                                    key={index}
+                                    title={items.title}
+                                    imageUrl={items.image_url}
+                                    description={items.description}
+                                    showHandle={this.props.showHandle} />
+                            )
+                        }
+                        )}
+                    </>
+                </CardColumns>
+                </div>
+
+
+            )
+        }
+    }
+
+    export default Main;
